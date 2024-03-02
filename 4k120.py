42f009ba0cfbcc6b741694f887acff9f830ff08f
Danny Stewart <danny@stewart.cc>
2024-03-02 14:12:20 -0500

    4k120.py (feature): Add check for nircmd accessibility in system PATH

--- 4k120.py
+++ 4k120.py
@@ -10,9 +10,24 @@ REFRESH_RATE = 120
 NIRCMD_PATH = r"C:\Users\danny\OneDrive\Documents\Tech\Windows\nircmd-x64\nircmd.exe"
 
 
+def check_for_nircmd():
+    """Check if nircmd is accessible from the system's PATH."""
+    try:
+        subprocess.run(
+            ["nircmd", "wait", "1"],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
 def main():
-    """Main function"""
-    command = f"{NIRCMD_PATH} setdisplay {WIDTH} {HEIGHT} {COLOR_DEPTH} {REFRESH_RATE}"
+    """Main function."""
+    nircmd_path = "nircmd" if check_for_nircmd() else NIRCMD_PATH
+    command = f"{nircmd_path} setdisplay {WIDTH} {HEIGHT} {COLOR_DEPTH} {REFRESH_RATE}"
     print("Setting resolution to 4k120... ", end="")
     subprocess.run(command, shell=True)
     print("Done!")
